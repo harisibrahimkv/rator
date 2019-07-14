5eb98e5d113ad80a2a991f407c66195095d43768
Haris Ibrahim K V <blucalvin@gmail.com>
2019-07-14 11:26:03 +0530

    Creates the ExchangeRate model and adds migrations

--- scraper/models.py
+++ scraper/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class ExchangeRate(models.Model):
+    currency = models.CharField(null=True, blank=True, max_length=5)
+    rate = models.FloatField(null=True, blank=True)
+
+    def __str__(self):
+        return self.currency + '-' + str(self.rate)
